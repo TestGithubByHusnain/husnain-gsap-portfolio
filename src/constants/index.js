b25fb3239483f4bac4171c20e26f65123bc2de16
Husnain <husnainali41940@gmail.com>
2025-07-12 00:50:43 -0700

    Ready for Deployment.

--- src/constants/index.js
+++ src/constants/index.js
@@ -153,11 +153,11 @@ export const projects = [
 }
 ]
 export const socials = [
-  { name: "Instagram", href: "https://www.instagram.com/ali.sanatidev/reels/" },
-  {
-    name: "Youtube",
-    href: "https://www.youtube.com/channel/UCZhtUWTtk3bGJiMPN9T4HWA",
-  },
-  { name: "LinkedIn", href: "https://www.linkedin.com/in/ali-sanati/" },
-  { name: "GitHub", href: "https://github.com/Ali-Sanati" },
+  { name: "Instagram", href: "https://www.instagram.com/husnain_akram3" },
+  // {
+  //   name: "Youtube",
+  //   href: "https://www.youtube.com/channel/UCZhtUWTtk3bGJiMPN9T4HWA",
+  // },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/husnain-ali-a11813282/" },
+  { name: "GitHub", href: "https://github.com/TestGithubByHusnain" },
 ];
